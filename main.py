ecdbc3e357e905ac8a6934348507d2d8c3c44312
Hardik Jumnani <hardik.jumnani123@gmail.com>
2025-02-13 15:47:41 +0530

    canvas menu, select tools

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import cv2
 import math
 import mediapipe as mp
 from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmark
+import numpy as np
 import threading
 from threading import Thread
 import time
-from typing import Tuple
+from typing import Tuple, Dict, List, Literal
 
 pinch: float = 0.0 # 0.0 = open, 0.5 <= pinch
 
 PINCH_RECORDED: bool = False
 pinch_dist: float = 0.0
+
+CANVAS_OPEN: bool = False
+TOOL: Literal["selector", "rectangle", "circle", "line"] = "" # selector, rectangle, circle, line
+DRAWING: bool = False
 
 def pixelize_coords(x: float, y: float) -> Tuple[int]:
     h, w, _ = frame.shape
@@ -20,6 +25,80 @@ def distance(coord_1: NormalizedLandmark, coord_2: NormalizedLandmark) -> int:
     x1, y1, z1 = coord_1.x, coord_1.y, coord_1.z
     x2, y2, z2 = coord_2.x, coord_2.y, coord_2.z
     return math.sqrt( (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2 )
+
+def is_finger_within_box(finger_point: NormalizedLandmark, top_left_coord: Tuple[int], bottom_right_coord: Tuple[int]) -> bool:
+    
+    finger_x, finger_y = pixelize_coords(finger_point.x, finger_point.y)
+    x_between_item: bool = finger_x >= top_left_coord[0] and finger_x <= bottom_right_coord[0]
+    y_between_item: bool = finger_y >= top_left_coord[1] and finger_y <= bottom_right_coord[1]
+    if x_between_item and y_between_item:
+        return True
+    else:
+        return False
+
+HUD_ITEMS: Dict[str, List[Tuple[int, int]]] = {
+    "three_lines": [((15, 20), (50, 40)), False], # coords, highlighted?
+    "selector": [((10, 10), (40, 50)), False],
+    "rectangle": [((50, 10), (90, 50)), False],
+    "circle": [((110, 10), (150, 50)), False],
+    "line": [((160, 10), (200, 50)), False],
+}
+def draw_hud(frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
+    if not CANVAS_OPEN:
+        cv2.line(frame, (15, 20), (50, 20), (14, 14, 14), 4)
+        cv2.line(frame, (15, 30), (50, 30), (14, 14, 14), 4)
+        cv2.line(frame, (15, 40), (50, 40), (14, 14, 14), 4)
+        return frame
+
+    # selector
+    pts = np.array([[10, 10], [10, 50], [20, 40], [38, 43]], np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 0), thickness=2)
+
+    # rectangle
+    cv2.rectangle(frame, (52, 12), (88, 48), (0, 0, 0), 2, cv2.LINE_AA)
+
+    # circle
+    cv2.circle(frame, (130, 30), 20, (0, 0, 0), 2, cv2.LINE_AA)
+
+    # line
+    cv2.line(frame, (162, 12), (198, 48), (0, 0, 0), thickness=2)
+
+    return frame
+
+def highlight_hud(frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
+    HIGHLIGHT_MARGIN = 10
+    global results
+    if not results: return frame
+    if not results.multi_hand_landmarks: return frame
+
+    hand_landmarks = results.multi_hand_landmarks[0]
+    _, index_tip = hand_landmarks.landmark[4], hand_landmarks.landmark[8]
+
+    for hud_name, item in HUD_ITEMS.items():
+        # Three Lines
+        if hud_name == "three_lines" and not CANVAS_OPEN:
+            hx1, hy1 = item[0][0][0]-HIGHLIGHT_MARGIN, item[0][0][1]-HIGHLIGHT_MARGIN
+            hx2, hy2 = item[0][1][0]+HIGHLIGHT_MARGIN, item[0][1][1]+HIGHLIGHT_MARGIN
+            
+            if is_finger_within_box(index_tip, (hx1, hy1), (hx2, hy2)):
+                cv2.rectangle(frame, (hx1, hy1), (hx2, hy2), (0, 0, 255), 2)
+                item[1] = True
+            else:
+                item[1] = False
+        
+        # All elements
+        elif CANVAS_OPEN and not hud_name == "three_lines":
+            hx1, hy1 = item[0][0][0]-HIGHLIGHT_MARGIN+5, item[0][0][1]-HIGHLIGHT_MARGIN+5
+            hx2, hy2 = item[0][1][0]+HIGHLIGHT_MARGIN-5, item[0][1][1]+HIGHLIGHT_MARGIN-5
+            
+            if is_finger_within_box(index_tip, (hx1, hy1), (hx2, hy2)):
+                cv2.rectangle(frame, (hx1, hy1), (hx2, hy2), (0, 0, 255), 2)
+                item[1] = True
+            else:
+                item[1] = False
+
+    return frame
 
 def record_pinch() -> None:
     global PINCH_RECORDED, pinch_dist, results
@@ -38,14 +117,46 @@ def record_pinch() -> None:
     else:
         print("Hand not found!")
 
-def check_pinch() -> None:
-    global pinch
-    dist_index_thumb: int = distance(hand_landmarks.landmark[4], hand_landmarks.landmark[8])
+def check_pinch() -> bool:
+    global results, pinch
+    hand_landmarks = results.multi_hand_landmarks[0]
+    thumb_tip, index_tip = hand_landmarks.landmark[4], hand_landmarks.landmark[8]
+    dist_index_thumb: int = distance(thumb_tip, index_tip)
+
     if dist_index_thumb < pinch_dist: pinch = 1.0
     else: pinch = max(pinch-0.5, 0.0)
 
-    if pinch == 0.0: print("pinch-open")
-    else: print("pinch-pinched")
+    if pinch == 0.0: return False
+    else: return True
+
+def action_on_pinch(frame: cv2.typing.MatLike) -> None:
+    global CANVAS_OPEN, DRAWING, TOOL, results
+
+    if not results or not results.multi_hand_landmarks: return None
+    if not check_pinch(): return None
+
+    index_tip = results.multi_hand_landmarks[0].landmark[8]
+
+    # Three Lines
+    if not CANVAS_OPEN:
+        if is_finger_within_box(index_tip, HUD_ITEMS["three_lines"][0][0], HUD_ITEMS["three_lines"][0][1]):
+            CANVAS_OPEN = True
+            DRAWING = False
+            TOOL = ""
+    elif CANVAS_OPEN and not DRAWING:
+        for hud_name, item in HUD_ITEMS.items():
+            if hud_name == "three_lines": continue
+            if is_finger_within_box(index_tip, item[0][0], item[0][1]):
+                DRAWING = True
+                TOOL = hud_name
+                CANVAS_OPEN = False
+    
+    elif DRAWING:
+        if TOOL == "selector": ...
+        elif TOOL == "rectangle": ...
+
+        elif TOOL == "circle": ...
+        elif TOOL == "line": ...
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
@@ -89,6 +200,9 @@ while cap.isOpened():
             
 
     # Display the frame
+    frame = draw_hud(frame)
+    frame = highlight_hud(frame)
+    action_on_pinch(frame)
     cv2.imshow("MediaPipe Hands", frame)
 
     # Press 'q' to exit
